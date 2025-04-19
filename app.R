a25e8f0a220039fe276e199e284c1d92e4eafc28
Anthony Sena <asena5@its.jnj.com>
2025-04-19 09:04:02 -0400

    Initial setup

---
+++ app.R
@@ -0,0 +1,46 @@
+library(OhdsiShinyModules)
+library(ShinyAppBuilder)
+
+shinyConfig <- initializeModuleConfig() |>
+  addModuleConfig(
+    createDefaultAboutConfig()
+  )  |>
+  addModuleConfig(
+    createDefaultDatasourcesConfig()
+  )  |>
+  addModuleConfig(
+    createDefaultCohortGeneratorConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultCohortDiagnosticsConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultCharacterizationConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultPredictionConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultEstimationConfig()
+  ) 
+
+
+cli::cli_h1("Starting shiny server")
+serverStr <- paste0(Sys.getenv("shinydbServer"), "/", Sys.getenv("shinydbDatabase"))
+cli::cli_alert_info("Connecting to {serverStr}")
+connectionDetails <- DatabaseConnector::createConnectionDetails(
+  dbms = "postgresql",
+  server = serverStr,
+  port = Sys.getenv("shinydbPort"),
+  user = "shinyproxy",
+  password = Sys.getenv("shinydbPw")
+)
+
+cli::cli_h2("Loading schema")
+createShinyApp(
+   config = shinyConfig,
+   connectionDetails = connectionDetails,
+   resultDatabaseSettings = createDefaultResultDatabaseSettings(schema = "ohdsiexamplestudy"),
+   title = "OHDSI Example Study"
+   description = "The OHDSI Example Study is designed to run across the OHDSI Evidence Network to provide a way to test Strategus and all HADES modules."
+)
